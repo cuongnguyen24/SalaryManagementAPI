9c31765a9a5b8f3a057083941e7f3914f96d3b31
cuongnguyen24 <cuongngba7@gmail.com>
2025-07-16 18:36:55 +0700

    update datetime

--- SalaryManagementAPI/Helpers/AutoMapperProfile.cs
+++ SalaryManagementAPI/Helpers/AutoMapperProfile.cs
@@ -6,7 +6,11 @@
         {
             CreateMap<GiamTruDTO, GiamTruThueTNCN>().ReverseMap();
             CreateMap<HopDong, HopDongDTO>().ReverseMap();
-            CreateMap<NhanVien, NhanVienDTO>().ReverseMap();
+            //CreateMap<NhanVien, NhanVienDTO>().ReverseMap();
+            CreateMap<NhanVien, NhanVienDTO>().ReverseMap()
+            .ForMember(dest => dest.NgaySinh,
+                opt => opt.MapFrom(src => DateTime.SpecifyKind(src.NgaySinh, DateTimeKind.Utc)));
+
             CreateMap<PhongBan, PhongBanDTO>().ReverseMap();
             CreateMap<ChucVu, ChucVuDTO>().ReverseMap();
             CreateMap<CapNhatThongTinCaNhanDTO, NhanVien>()
